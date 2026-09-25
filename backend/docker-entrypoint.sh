e332f2a4fcc11b49391065d3bc06479fc2eb155d
SORO LAMOUSSA <sorolamoussa1212@gmail.com>
2026-09-25 20:28:07 +0000

    fix: bypass composer scripts during docker build

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 # Run optimizations & migrations in production
 if [ "$APP_ENV" = "production" ]; then
     echo "Running production setup..."
+    php artisan package:discover --ansi || true
     php artisan config:cache || true
     php artisan route:cache || true
     php artisan view:cache || true
